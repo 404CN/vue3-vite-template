2ba3f8afd3fed85008141ec04b9b4868ccd56a08
404 <1063974339@qq.com>
2025-02-28 22:18:35 +0800

    feat:新增pinia持久化

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,14 +1,28 @@
 import { ref, reactive, computed } from 'vue'
 import { defineStore } from 'pinia'
+// import { parse, stringify } from 'zipson'
 
-export const useAppStore = defineStore('app', () => {
-  const app = ref('Vue 3 + Vite + Pinia + Ts')
+export const useAppStore = defineStore(
+  'app',
+  () => {
+    const app = ref('Vue 3 + Vite + Pinia + Ts')
 
-  const isDarkTheme = ref(false);
+    const isDarkTheme = ref(false);
 
-  const setTheme = () => {
-    isDarkTheme.value = !isDarkTheme.value;
+    const setTheme = () => {
+      isDarkTheme.value = !isDarkTheme.value;
+    }
+
+    return { app, isDarkTheme, setTheme }
+  },
+  {
+    persist: {
+      key: 'app',
+      storage: localStorage,
+      // serializer: {
+      //   deserialize: parse,
+      //   serialize: stringify
+      // }
+    },
   }
-
-  return { app, isDarkTheme, setTheme }
-})
+)
